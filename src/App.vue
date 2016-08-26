6b3238b9e35e60f191db22086f3c000d1840ad71
Awe <hilongjw@gmail.com>
2016-08-26 19:26:04 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <rd-button>确定</rd-button>
+    <rd-loadingbar></rd-loadingbar>
+    <rd-drop-button text=操作>
+      <rd-button>编辑</rd-button>
+      <rd-button>审核</rd-button>
+      <rd-button>删除</rd-button>
+  </rd-drop-button>
+  </div>
+</template>
+
+<script>
+// radon
+import 'radon-ui/dist/static/css/dist.css'
+
+import {
+  rdButton,
+  rdDropButton
+} from 'radon-ui'
+
+export default {
+  ready () {
+    this.$Progress.start()
+  },
+  components: {
+    rdButton,
+    rdDropButton
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App'
+import RadonUI from 'radon-ui/dist/components/install'
+
+Vue.use(RadonUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
+})
